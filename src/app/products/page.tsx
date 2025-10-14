5ed95710f6ab6f60cef3236c03680e4f81b725a6
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2025-10-13 21:31:20 -0400

    feat: adicionando a biblioteca shadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const pageProducts = () => {
+  return (
+    <div>
+      <h1>Página de produtos</h1>
+      <Button>Teste</Button>
+    </div>
+  );
+};
+
+export default pageProducts;
